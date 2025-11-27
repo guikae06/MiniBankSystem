8223492596481d453c248e7a358cfb95bab7b24d
guiake <kaelig.guillemette@gmail.com>
2025-11-27 21:28:02 +0100

    added Authmanager.h DBManager.h and StockMarket.h

---
+++ headers/Authmanager.h
@@ -0,0 +1,10 @@
+#ifndef AUTHMANAGER_H
+#define AUTHMANAGER_H
+
+class AuthManager
+{
+public:
+    AuthManager();
+};
+
+#endif // AUTHMANAGER_H
---
+++ headers/StockMarket.h
@@ -0,0 +1,10 @@
+#ifndef STOCKMARKET_H
+#define STOCKMARKET_H
+
+class StockMarket
+{
+public:
+    StockMarket();
+};
+
+#endif // STOCKMARKET_H
