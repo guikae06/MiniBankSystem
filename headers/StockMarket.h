fb7fee82ac037c65ee074d79db75efabbd75b6de
guiake <kaelig.guillemette@gmail.com>
2025-11-27 22:33:01 +0100

    added the stockmarket.h

--- headers/StockMarket.h
+++ headers/StockMarket.h
@@ -1,10 +1,30 @@
 #ifndef STOCKMARKET_H
 #define STOCKMARKET_H
 
-class StockMarket
-{
+#include <QObject>
+#include <QTimer>
+#include <QMap>
+#include <QList>
+
+struct StockInfo { QString symbol; QString name; double price; };
+
+class StockMarket : public QObject {
+    Q_OBJECT
 public:
-    StockMarket();
+    explicit StockMarket(QObject *parent=nullptr);
+    void start(int intervalMs=2000);
+    QList<StockInfo> stocks() const;
+
+signals:
+    void priceUpdated(const QString &symbol, double price);
+
+public slots:
+    void tick();
+
+private:
+    QTimer m_timer;
+    QMap<QString, StockInfo> m_stocks;
+    double randomJitter();
 };
 
-#endif // STOCKMARKET_H
+#endif
