2a8f13c45f70797002761a8cefcf1f2bbfb0fc6b
guiake <kaelig.guillemette@gmail.com>
2025-11-27 22:33:11 +0100

    added Auth

--- headers/Authmanager.h
+++ headers/Authmanager.h
@@ -1,10 +1,20 @@
 #ifndef AUTHMANAGER_H
 #define AUTHMANAGER_H
 
-class AuthManager
-{
+#include <QString>
+#include "FileStorage.h"
+
+class AuthManager {
 public:
-    AuthManager();
+    explicit AuthManager(FileStorage *storage);
+    bool signup(const QString &username, const QString &password, QString &err);
+    bool login(const QString &username, const QString &password, int &userId, QString &err);
+
+private:
+    FileStorage *m_storage;
+    QString generateSalt() const;
+    QString hashPassword(const QString &password, const QString &salt) const;
+    int nextUserId(const QJsonArray &users) const;
 };
 
 #endif // AUTHMANAGER_H
